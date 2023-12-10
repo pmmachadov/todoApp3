b047536180e5737dcb8a77df782efd92a6e8336d
pmmachadov <pmmachadov@gmail.com>
2023-12-10 19:55:52 +0100

    Code totally changed to English and css modified

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.addEventListener("DOMContentLoaded", displayTasks);
+// READ:
 
+// Display tasks in the HTML container
 function displayTasks() {
   const tasksContainer = document.getElementById("tasks-container");
-  tasksContainer.innerHTML = "";
+  tasksContainer.innerHTML = ""; // Clear the container
 
-  const tasks = getTasks();
+  const tasks = getTasks(); // Get tasks stored locally
 
   tasks.forEach(task => {
     const taskDiv = document.createElement("div");
@@ -12,7 +13,7 @@ function displayTasks() {
 
     taskDiv.innerHTML = `
       <span class="task">${task}</span>
-      <button class="delete-btn" onclick="deleteTask('${task}')">Eliminar</button>
+      <button class="delete-btn" onclick="deleteTask('${task}')">Delete</button>
     `;
 
     tasksContainer.appendChild(taskDiv);
@@ -20,30 +21,65 @@ function displayTasks() {
 }
 
 function getTasks() {
-  const tasksString = localStorage.getItem("tasks");
-  return tasksString ? JSON.parse(tasksString) : [];
+  const tasksString = localStorage.getItem("tasks");  // READ: Get tasks stored locally
+  return tasksString ? JSON.parse(tasksString) : []; // If tasksString is null, return an empty array. JSON.parse(tasksString) is used here to convert the string to an array because localStorage only stores strings.
 }
 
+
+
+
+
+
+// CREATE:
+
+//Add a new task to local storage and update the display
 function addTask() {
   const taskInput = document.getElementById("taskInput");
   const task = taskInput.value.trim();
 
   if (task !== "") {
+    // READ: Get tasks stored locally
     const tasks = getTasks();
+
+    // CREATE: Add the new task to the array
     tasks.push(task);
+
+    // CREATE: Store the updated array in localStorage
     localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Clear the input
     taskInput.value = "";
+
+    // READ: Display the updated tasks
     displayTasks();
   }
 }
 
+
+
+
+
+
+// DELETE:
+
+//Remove a task from local storage and update the display
 function deleteTask(task) {
-  const tasks = getTasks().filter(t => t !== task);
+  // READ: Get tasks stored locally and filter out the task to delete
+  const tasks = getTasks().filter(t => t !== task); // READ: Get tasks stored locally and filter out the task to be deleted (filter returns a new array)
+
+  // DELETE: Store the updated array in localStorage
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  // READ: Display the updated tasks
   displayTasks();
 }
 
-// Agrega un evento de escucha para la tecla "Enter"
+
+
+
+
+
+// Add an event listener for the "Enter" key to facilitate task creation
 document.getElementById("taskInput").addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     addTask();
